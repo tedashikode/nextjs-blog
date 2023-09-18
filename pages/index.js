41c70ac557babc80afc0de86945c3fa77274e3ae
tedashikode <theodoremawuli77@gmail.com>
2023-09-18 01:50:59 +0000

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,46 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import Date from '../components/date';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+import { getSortedPostsData } from '../lib/posts';
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>I'm a budding web developer, aspring game dev and a neural network enthusiast 👩🏿‍💻</p>
+        <p>
+          Follow me as I share insights about my learnings 📑
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+          </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
